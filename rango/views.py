c9acb8caca944cbe0d0ac368f2273498d0a00fda
Dhairya Chhipani <2675767c@student.gla.ac.uk>
2022-02-13 23:37:15 +0000

    eighth commit

--- rango/views.py
+++ rango/views.py
@@ -21,14 +21,13 @@ def index(request):
 	return render(request, 'rango/index.html', context=context_dict)
 
 
-def about(request) :
-	# Construct a dictionary to pass to the template engine as its context.
-	# Note the key boldmessage matches to {{ boldmessage }} in the template!
-	context_dict = {'boldmessage': 'This tutorial has been put together by Dhairya Chhipani'}
-	# Return a rendered response to send to the client.
-	# We make use of the shortcut function to make our lives easier.
-	# Note that the first parameter is the template we wish to use.
-	return render(request, 'rango/about.html', context=context_dict)
+def about(request):
+	# prints out whether the method is a GET or a POST
+	print(request.method)
+	# prints out the user name, if no one is logged in it prints `AnonymousUser`
+	print(request.user)
+	return render(request, 'rango/about.html', {})
+
 
 def show_category(request, category_name_slug):
 	# Create a context dictionary which we can pass
