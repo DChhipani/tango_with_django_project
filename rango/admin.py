1e3d1eee7b6f3da3d0d143e74882bbe1b8eb5050
Dhairya Chhipani <2675767c@student.gla.ac.uk>
2022-02-13 20:28:44 +0000

    sixth commit

--- rango/admin.py
+++ rango/admin.py
@@ -3,7 +3,13 @@ from rango.models import Category, Page
 
 # Register your models here.
 
-admin.site.register(Category)
+# Add in this class to customise the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+	prepopulated_fields = {'slug':('name',)}
+
+# Update the registration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+
 
 class PageAdmin(admin.ModelAdmin):
 	list_display = ('title', 'category', 'url')
